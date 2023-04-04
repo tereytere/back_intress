f7a06fa3f5fc018910a753883944c5ce5742d857
kiaramm10 <kiaramalasquez5@gmail.com>
2023-04-04 14:05:23 +0200

    api update

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
