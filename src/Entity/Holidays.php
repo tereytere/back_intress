b7072388590ca1bdf95400ad2a542c31c69aacfa
tereytere <116809352+tereytere@users.noreply.github.com>
2023-04-10 09:32:21 +0200

    holidays api

--- src/Entity/Holidays.php
+++ src/Entity/Holidays.php
@@ -6,8 +6,11 @@ use App\Repository\HolidaysRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
+
 
 #[ORM\Entity(repositoryClass: HolidaysRepository::class)]
+#[ApiResource]
 class Holidays
 {
     #[ORM\Id]
