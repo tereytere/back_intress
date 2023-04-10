fde0cf9d6b7710cfac96e2e15b77ef97083e65eb
tereytere <116809352+tereytere@users.noreply.github.com>
2023-04-10 09:31:33 +0200

    documents api

--- src/Entity/Documents.php
+++ src/Entity/Documents.php
@@ -2,12 +2,14 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\DocumentsRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: DocumentsRepository::class)]
+#[ApiResource]
 class Documents
 {
     #[ORM\Id]
